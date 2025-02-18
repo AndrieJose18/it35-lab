554f58c38f10a2794cf584cc0d2e4cef7dc7c1bc
AndrieJose18 <20201270@nbsc.edu.ph>
2025-02-18 10:42:37 +0800

    Updates

--- assets/swipe-back-legacy-B6Mh_QGc.js
+++ assets/swipe-back-legacy-B6Mh_QGc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BazqjVTu.js","./index3-legacy-BD-Yg130.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-CL6Qwn8u.js","./index3-legacy-EG8SXjeo.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
