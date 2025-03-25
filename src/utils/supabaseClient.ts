5bfc0fc8c5362e969c01303a4cc739d924280dd5
AndrieJose18 <20201270@nbsc.edu.ph>
2025-03-25 23:28:40 +0800

    fixed some minor changes from the signup folder after adding the bycript and supabase and also adding utils under the src folder

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = "https://inonmsiqhaqpdvuzxknt.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlub25tc2lxaGFxcGR2dXp4a250Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3NzQzNDEsImV4cCI6MjA1ODM1MDM0MX0.21x1gTTPDXgCZXDMK_hLN56sUhkwZVngP0fgvKB6ulE";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
