93ae52a877dc37a2e63b1eeecddd39175e3c7382
AndrieJose18 <20201270@nbsc.edu.ph>
2025-04-01 21:10:24 +0800

    Updates

--- assets/md.transition-legacy-C2BGBL0W.js
+++ assets/md.transition-legacy-C2BGBL0W.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CLwqEw4Z.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-_YgG9X9Z.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-fDJwh5rs.js
+++ assets/swipe-back-legacy-fDJwh5rs.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CLwqEw4Z.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-_YgG9X9Z.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
